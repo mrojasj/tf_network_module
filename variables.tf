dc60c41b5298d505995d3aa7794c37fb94bc502a
EC2 Default User <ec2-user@ip-10-0-0-149.ec2.internal>
2021-05-07 22:55:15 +0000

    Second commit

--- variables.tf
+++ variables.tf
@@ -4,4 +4,8 @@ variable "vpc_cidr_block" {
 
 variable "snet_extra_bits" {
   type = number
+  validation {
+    condition = var.snet_extra_bits > 4
+    error_message = "The snet_extra_bits value must be equal or greater than 4"
+  }
 }
