57c82a96760d295f3824fbe73b6568c8bd75d83e
EC2 Default User <ec2-user@ip-10-0-0-149.ec2.internal>
2021-05-07 23:17:58 +0000

    Fix variables again

--- variables.tf
+++ variables.tf
@@ -6,6 +6,6 @@ variable "snet_extra_bits" {
   type = number
   validation {
     condition = var.snet_extra_bits >= 4
-    error_message = "The snet_extra_bits value must be equal or greater than 4"
+    error_message = "The snet_extra_bits value must be equal or greater than 4."
   }
 }
